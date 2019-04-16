40d44f49b6245758783e5d13168fa6975ff9c58a
Martin <stuurmartinpost@gmail.com>
2019-04-16 21:59:12 +0200

    added companies resource table

---
+++ companies/model.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../db')
+
+const Company = sequelize.define('companies', {
+  name: {
+    type: Sequelize.STRING,
+    field: 'name',
+    allowNull: false
+  },
+  foundingYear: {
+    type: Sequelize.INT,
+    field: 'founding_year',
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    field: 'description',
+    allowNull: 'description'
+  }
+}, {
+  timestamps: false,
+  tableName: 'companies'
+})
+
+module.exports = Company
