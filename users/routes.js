34296df81314e4c786d8ed19d0a2c5a706a8534d
Martin <stuurmartinpost@gmail.com>
2019-04-17 16:22:17 +0200

    made user posting possible

--- users/routes.js
+++ users/routes.js
@@ -1,27 +1,39 @@
-const {
-  Router
-} = require('express')
+const { Router } = require('express')
 const User = require('./model')
 
 const router = new Router()
 
-router.post('/user', (req, res, next) => {
+router.post('/users', (req, res, next) => {
+  console.log('req.body test:', req.body)
+
   User
-    .create(req.body)
-    .then(user => {
+  .create(req.body)
+  .then(user => {
+    if (!user) {
+      return res.status(404).send({
+          message: 'Not a user'
+      })
+    }
+    return res.status(201).send(user)
+   })
+   .catch(error => next(error))
 
-      const email = req.body.email
-      const password = req.body.password
+//   const  email = req.body.email
+//   const  password = req.body.password
 
-      if (!email || !password) {
-        res.status(400).send({
-          message: 'Please supply a valid email and password'
-        })
-      }
-      return res.status(201).send(user)
-
-    })
-    .catch(error => next(error))
+// if (!email || !password) {
+//   res.status(400).send({
+//     message: 'Please supply a valid email and password'
+//   })
+// }
+//   User
+//   .create(req.body)
+//   .then(user => {
+// console.log("faafsafsasdf",user)
+     
+//     return res.status(201).send(user)
+//   })
+//   .catch(error => next(error))
 })
 
 module.exports = router
